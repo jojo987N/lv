b2f00df03a1ec70a779bcd0ae88635468f75954c
nass <cabliveer@gmail.com>
2022-09-29 00:28:06 +0200

    gf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <iframe
       width="853"
       height="480"
-      src={"https://www.youtube.com/watch?v=rWIhYhZin3o"}
+      src={"https://www.youtube.com/embed/rWIhYhZin3o"}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
